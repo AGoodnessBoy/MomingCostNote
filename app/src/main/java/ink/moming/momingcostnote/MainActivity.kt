d388eb6a06751d6b5578dd4fbe12d2c217c9c5d1
默明 <moming@momingdeMacBook-Pro.local>
2019-11-26 08:05:14 +0800

    Initial commit

---
+++ app/src/main/java/ink/moming/momingcostnote/MainActivity.kt
@@ -0,0 +1,17 @@
+package ink.moming.momingcostnote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
